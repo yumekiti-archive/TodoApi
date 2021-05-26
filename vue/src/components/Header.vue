00bfc7126261be8564280adbc0dad5770b714ec5
yumekiti <yumekiti120440@gmail.com>
2021-05-27 02:06:00 +0900

    add: login

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-app-bar color="blue">
+
+            <v-app-bar-nav-icon @click="drawer = true">
+                
+            </v-app-bar-nav-icon>
+
+            <v-toolbar-title>Todo</v-toolbar-title>
+
+        </v-app-bar>
+        
+
+        <v-navigation-drawer
+            v-model="drawer"
+            fixed
+            temporary
+        >
+            <v-list nav>
+                <v-list-item-group>
+
+                    <router-link to="/">
+                        <v-list-item>
+                            ホーム
+                        </v-list-item>
+                    </router-link>
+
+                    <router-link to="/login">
+                        <v-list-item>
+                            ログイン
+                        </v-list-item>
+                    </router-link>
+
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+            
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        data () {
+            return {
+                drawer: false,
+            }
+        }
+    }
+</script>
---
+++ vue/src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-app>
+        
+        <Header />
+
+        <v-card-title class="mx-auto mt-5">
+            <h1 class="display-1">ログインフォーム</h1>
+        </v-card-title>
+
+        <v-card width="400px" class="mx-auto mt-5">
+            <v-card-text>
+                <v-form>
+                    <v-text-field
+                        prepend-icon="mdi-account-circle"
+                        label="メールアドレス"
+                        v-model="mailaddress"
+                    />
+                    <v-text-field
+                        v-bind:type="showPassword ? 'text' : 'password'"
+                        @click:append="showPassword = !showPassword"
+                        prepend-icon="mdi-lock" 
+                        v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        label="パスワード"
+                        v-model="password"
+                    />
+                    <v-card-actions>
+                        <v-btn class="info" @click="submit">ログイン</v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+    name: 'Login',
+    components: {
+        Header
+    },
+    data(){
+        return {
+            showPassword : false,
+            mailaddress:'',
+            password:'',
+        }
+    },
+    methods:{
+        submit(){
+            console.log(this.mailaddress,this.password)
+        }
+    }
+}
+</script>
